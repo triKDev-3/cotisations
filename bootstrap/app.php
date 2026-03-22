9efc23d21292428806df152c7d353f1be19756ba
Ecolkkk <kodokoffikevin@gmail.com>
2026-03-22 22:47:28 +0000

    Fix: trusted proxies configuration for Render (419 page expired issue)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'collecteur' => \App\Http\Middleware\EnsureUserIsCollecteurOrAdmin::class,
         ]);
