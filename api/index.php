14a21b2002322671a1a605135a6ac748d4016ec0
Ecolkkk <kodokoffikevin@gmail.com>
2026-03-26 01:45:01 +0000

    fix: app/index.php for Vercel SSL

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+// Vercel only allows writing to /tmp
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+
+// Force use of SSL on Vercel
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 require __DIR__ . '/../public/index.php';
