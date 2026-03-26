f4daaf2325da61ac729218a3cb67eee59842498c
Ecolkkk <kodokoffikevin@gmail.com>
2026-03-26 07:02:28 +0000

    fix: re-enable laravel with safety check

--- api/index.php
+++ api/index.php
@@ -1,10 +1,17 @@
 <?php
-echo "Hello from Vercel PHP!"; die();
+
+// Check for vendor/autoload
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    echo "Composer dependencies are not installed on Vercel!";
+    die();
+}
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Vercel only allows writing to /tmp
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
 
 // Force use of SSL on Vercel
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
