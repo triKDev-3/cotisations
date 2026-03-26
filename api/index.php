a8331c6960ec1c2631373b9ba65d97ef18c8927f
Ecolkkk <kodokoffikevin@gmail.com>
2026-03-26 19:28:16 +0000

    fix: final api entrypoint attempt

--- api/index.php
+++ api/index.php
@@ -6,16 +6,10 @@ if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
     die();
 }
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-// Vercel only allows writing to /tmp
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
-$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
-
-// Force use of SSL on Vercel
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
-}
+// Laravel needs a writable folder for storage
+// Vercel only provides /tmp
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
 
 require __DIR__ . '/../public/index.php';
